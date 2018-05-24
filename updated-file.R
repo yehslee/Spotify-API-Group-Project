b71c19a750a4dd637f6fdf2a0202c4aeb6b1756b
Alvin Tran <vietra@uw.edu>
2018-05-23 21:22:11 -0700

    fixed to add data frame of like & dislike

--- updated-file.R
+++ updated-file.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(stringr)
 source("api-key.R")
 
-username <- "thealvintran"
+username <- "buzzfeedvideo"
 youtuber <- GET(paste0("https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=", username, "&key=", google_key))
 youtuber_content <- content(youtuber, "text")
 youtuber_body <- fromJSON(youtuber_content)
@@ -18,13 +18,15 @@ temp_df <- flatten(temp_body$items$contentDetails)
 
 video_id <- temp_df$videoId
 
-datalist = list()
+like_list = data.frame("like"=1,"dislike"=1)[0,]
 for (i in 1:50) {
   loop <- GET(paste0("https://www.googleapis.com/youtube/v3/videos?id=", video_id[[i]],"&key=", google_key,"&part=statistics"))
   loop_content <- content(loop, "text")
   loop_body <- fromJSON(loop_content)
   loop_stats <- flatten(loop_body$items$statistics)
+  like <- loop_stats$likeCount
   dislikes <- loop_stats$dislikeCount
-  datalist[i] <- dislikes # add it to your list
+  like_list[[i,1]] <- like # add it to your list
+  like_list[[i,2]] <- dislikes
 }
-# GOT LIST OF DISLIKES NUMBERS
+# GOT DATAFRAME OF LIKES & DISLIKES NUMBERS
