c6e3313ae6747614415b5d8b7de59ff8ae41dbaf
Chris-Vitalis <cvitalis@uw.edu>
2018-05-26 17:50:31 -0700

    Make playlists Functions

--- playlist_plot.R
+++ playlist_plot.R
@@ -1,40 +1,61 @@
 source("key.R")
 library(lubridate)
 library(dplyr)
-#name <- "jojo_saunders"
-#playlist_name <-get_user_audio_features(name)
-
-playlist_features_parse <- function(name){
-  playlist_name <-get_user_audio_features(name)
+dance <- function(name){
+  playlist_info <-get_user_audio_features(name)
   
-  playlist_name %>% 
+  playlist_info <- playlist_info %>% 
   select(playlist_name, track_name, artist_name, album_name, track_popularity,
          energy, key, loudness, mode, speechiness, acousticness,
          instrumentalness, liveness, valence, tempo, duration_ms, 
          time_signature, key_mode, danceability)
 
-custom_dance <- playlist_name %>% 
+custom_dance <- playlist_info %>% 
   filter(danceability > .7) %>% 
   filter(energy > .5) %>% 
   select(playlist_name, track_name, artist_name, album_name, danceability, 
          energy) %>% 
   arrange(-danceability)
 
-custom_chill <- playlist_name %>% 
-  filter(energy < .5) %>%
-  filter(acousticness > .5) %>% 
-  select(playlist_name, track_name, artist_name, album_name, 
-         energy, acousticness) %>% 
-  arrange(energy)
+return(custom_dance)
+}
 
-custom_simp <- playlist_name %>% 
-  filter(valence < .3) %>%
-  filter(energy < .3) %>% 
-  select(playlist_name, track_name, artist_name, album_name, 
-         energy, valence) %>% 
-  arrange(valence)
+chill <- function(name){
+  playlist_info <-get_user_audio_features(name)
+  
+  playlist_info <- playlist_info %>% 
+    select(playlist_name, track_name, artist_name, album_name, track_popularity,
+           energy, key, loudness, mode, speechiness, acousticness,
+           instrumentalness, liveness, valence, tempo, duration_ms, 
+           time_signature, key_mode, danceability)
+  custom_chill <- playlist_info %>% 
+    filter(energy < .5) %>%
+    filter(acousticness > .5) %>% 
+    select(playlist_name, track_name, artist_name, album_name, 
+           energy, acousticness) %>% 
+    arrange(energy)
+  
+  
+  return(custom_chill)
 }
 
 
-
-
+simp <- function(name){
+  playlist_info <-get_user_audio_features(name)
+  
+  playlist_info <- playlist_info %>% 
+    select(playlist_name, track_name, artist_name, album_name, track_popularity,
+           energy, key, loudness, mode, speechiness, acousticness,
+           instrumentalness, liveness, valence, tempo, duration_ms, 
+           time_signature, key_mode, danceability)
+  
+  custom_simp <- playlist_info %>% 
+    filter(valence < .3) %>%
+    filter(energy < .3) %>% 
+    select(playlist_name, track_name, artist_name, album_name, 
+           energy, valence) %>% 
+    arrange(valence)
+  
+  
+  return(custom_simp)
+}
