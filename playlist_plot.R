26c99e611bce6e7050086db7f7b2825749b99532
yehslee <yehslee@uw.edu>
2018-05-26 18:11:12 -0700

    Make lines of code even smaller and simpler.

--- playlist_plot.R
+++ playlist_plot.R
@@ -4,31 +4,19 @@ library(dplyr)
 
 # Creates a table of playlists that is "danceable"
 dance <- function(name){
-  playlist_info <-get_user_audio_features(name) %>%
-    select(playlist_name, track_name, artist_name, album_name, track_popularity,
-           energy, key, loudness, mode, speechiness, acousticness,
-           instrumentalness, liveness, valence, tempo, duration_ms, 
-           time_signature, key_mode, danceability)
-
-custom_dance <- playlist_info %>% 
-  filter(danceability > .7) %>% 
-  filter(energy > .5) %>% 
-  select(playlist_name, track_name, artist_name, album_name, danceability, 
-         energy) %>% 
-  arrange(-danceability)
-
-return(custom_dance)
+  custom_dance <- get_user_audio_features(name) %>% 
+    filter(danceability > .7) %>% 
+    filter(energy > .5) %>% 
+    select(playlist_name, track_name, artist_name, album_name, danceability, 
+           energy) %>% 
+    arrange(-danceability)
+  
+  return(custom_dance)
 }
 
 # Creates a table of "chill" playlists
 chill <- function(name){
-  playlist_info <-get_user_audio_features(name) %>%
-    select(playlist_name, track_name, artist_name, album_name, track_popularity,
-           energy, key, loudness, mode, speechiness, acousticness,
-           instrumentalness, liveness, valence, tempo, duration_ms, 
-           time_signature, key_mode, danceability)
-
-  custom_chill <- playlist_info %>% 
+  custom_chill <- get_user_audio_features(name) %>% 
     filter(energy < .5) %>%
     filter(acousticness > .5) %>% 
     select(playlist_name, track_name, artist_name, album_name, 
@@ -40,13 +28,7 @@ chill <- function(name){
 
 # Creates a table of "simp" playlists
 simp <- function(name){
-  playlist_info <-get_user_audio_features(name) %>%
-    select(playlist_name, track_name, artist_name, album_name, track_popularity,
-           energy, key, loudness, mode, speechiness, acousticness,
-           instrumentalness, liveness, valence, tempo, duration_ms, 
-           time_signature, key_mode, danceability)
-  
-  custom_simp <- playlist_info %>% 
+  custom_simp <- get_user_audio_features(name) %>% 
     filter(valence < .3) %>%
     filter(energy < .3) %>% 
     select(playlist_name, track_name, artist_name, album_name, 
