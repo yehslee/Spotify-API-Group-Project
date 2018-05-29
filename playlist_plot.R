b05077b5205c20fc6ece4002f1eaf622c0e61fc5
yehslee <yehslee@uw.edu>
2018-05-29 14:06:20 -0700

    Small lint fixes

--- playlist_plot.R
+++ playlist_plot.R
@@ -4,39 +4,42 @@ library(dplyr)
 
 # Creates a table of playlists that is "danceable"
 dance <- function(name){
-  custom_dance <- get_user_audio_features(name) %>% 
-    filter(danceability > .7) %>% 
-    filter(energy > .5) %>% 
-    select(playlist_name, track_name, artist_name, album_name, danceability, 
+  custom_dance <- get_user_audio_features(name) %>%
+    filter(danceability > .7) %>%
+    filter(energy > .5) %>%
+    select(playlist_name, track_name, artist_name, album_name, danceability,
            energy) %>%
     arrange(-danceability)
-  colnames(custom_dance) <- c("Playlist", "Song", "Artist", "Album", "Dance Level" ,"Energy Level")
-  
+  colnames(custom_dance) <- c("Playlist", "Song", "Artist", "Album",
+                              "Dance Level", "Energy Level")
+
   return(head(custom_dance, n = 20))
 }
 
 # Creates a table of "chill" playlists
 chill <- function(name){
-  custom_chill <- get_user_audio_features(name) %>% 
+  custom_chill <- get_user_audio_features(name) %>%
     filter(energy < .5) %>%
-    filter(acousticness > .5) %>% 
-    select(playlist_name, track_name, artist_name, album_name, 
-           energy, acousticness) %>% 
+    filter(acousticness > .5) %>%
+    select(playlist_name, track_name, artist_name, album_name,
+           energy, acousticness) %>%
     arrange(energy)
-  colnames(custom_chill) <- c("Playlist", "Song", "Artist", "Album", "Energy Level" ,"Acousticness")
-  
+  colnames(custom_chill) <- c("Playlist", "Song", "Artist", "Album",
+                              "Energy Level", "Acousticness")
+
   return(head(custom_chill, n = 20))
 }
 
 # Creates a table of "simp" playlists
 simp <- function(name){
-  custom_simp <- get_user_audio_features(name) %>% 
+  custom_simp <- get_user_audio_features(name) %>%
     filter(valence < .3) %>%
-    filter(energy < .3) %>% 
-    select(playlist_name, track_name, artist_name, album_name, 
-           energy, valence) %>% 
+    filter(energy < .3) %>%
+    select(playlist_name, track_name, artist_name, album_name,
+           energy, valence) %>%
     arrange(valence)
-  colnames(custom_simp) <- c("Playlist", "Song", "Artist", "Album", "Energy Level" ,"Valence")
-  
+  colnames(custom_simp) <- c("Playlist", "Song", "Artist", "Album",
+                             "Energy Level", "Valence")
+
   return(head(custom_simp, n = 20))
 }
