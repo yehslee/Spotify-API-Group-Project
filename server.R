39288ded63bd05832b4611f7b73ddd77a368ef56
yehslee <yehslee@uw.edu>
2018-05-29 13:19:55 -0700

    Lint server file

--- server.R
+++ server.R
@@ -3,31 +3,35 @@ library(plotly)
 library(devtools)
 library(shiny)
 
-# devtools::install_github("ropensci/plotly")
-
 source("playlist_plot.R")
 
 # Creates a visual table using Spotify branding colors
 # and the columns can be manually moved
-createTable <- function(df, tableHeight = 50){
-  nms <- lapply(names(df), function(x){
+create_table <- function(df, table_height = 20) {
+  nms <- lapply(names(df), function(x) {
     return(paste0("<b>", x, "</b>"))
   })
-  
+
   nms <- append(nms, "<b>Ranking</b>", after = 0)
-  headerValues <- lapply(nms, function(x){return(list(x))})
-  
+  header_values <- lapply(nms, function(x) {
+    return(list(x))
+  })
+
   # Cell Values
   names(df) <- NULL
-  cellValues <- apply(df, 2, function(x){return(list(x))})
-  cellValues <- lapply(cellValues, function(x){return(unlist(x))})
-  
-  cellValues <- append(cellValues, list(rownames(df)), after = 0)
-  
+  cell_values <- apply(df, 2, function(x) {
+    return(list(x))
+  })
+  cell_values <- lapply(cell_values, function(x) {
+    return(unlist(x))
+  })
+
+  cell_values <- append(cell_values, list(rownames(df)), after = 0)
+
   # Create the list to pass to plot_ly()
   header <- list(
-    values = headerValues,
-    
+    values = header_values,
+
     # Formatting
     line = list(color = "#b3b3b3"),
     align = c("left", rep("center", ncol(df))),
@@ -36,32 +40,34 @@ createTable <- function(df, tableHeight = 50){
   )
 
   cells <- list(
-    values = cellValues,
-    
+    values = cell_values,
+
     # Formatting
-    line = list(color = '#b3b3b3'),
-    align = c('left', rep('right', ncol(df))),
-    font = list(color = c('#1db954', rep("#ffffff", ncol(df))), size = 12),
+    line = list(color = "#b3b3b3"),
+    align = c("left", rep("right", ncol(df))),
+    font = list(color = c("#1db954", rep("#ffffff", ncol(df))), size = 12),
     fill = list(color = c("#212121", rep("#303030", ncol(df)))),
     height = 20
   )
   m <- list(
     pad = 0
   )
-  
+
   p <- plot_ly(
     type = "table",
     header = header,
     cells = cells,
     width = 900,
-    height = 1200) %>%
- 
-    layout(xaxis = list(zeroline = F, showgrid = F, showticklabels = F),
-           yaxis = list(zeroline = F, showgrid = F, showticklabels = F), 
-           plot_bgcolor='transparent',
-           paper_bgcolor='transparent', 
-           margin = m)
-  
+    height = 1200
+  ) %>%
+    layout(
+      xaxis = list(zeroline = F, showgrid = F, showticklabels = F),
+      yaxis = list(zeroline = F, showgrid = F, showticklabels = F),
+      plot_bgcolor = "transparent",
+      paper_bgcolor = "transparent",
+      margin = m
+    )
+
   return(p)
 }
 
@@ -70,35 +76,37 @@ source("alvin-analysis.R")
 
 df <- read.csv("data/artist_df.csv", stringsAsFactors = FALSE)
 
-shinyServer(function(input, output) { 
-  
-  output$text <- renderPrint({input$username})
-  
+shinyServer(function(input, output) {
+  output$text <- renderPrint({
+    input$username
+  })
+
   output$plot <- renderPlotly({
     data <- df %>%
       filter(artist == input$artist) %>%
       select(album_name, track_name, danceability, energy, liveness, valence)
     return(custom_plot(data, input$x_var, input$y_var))
   })
-  
+
   output$pop_plot <- renderPlotly({
-    data <- df %>% 
+    data <- df %>%
       filter(artist == input$artist2) %>%
-      select(album_name, track_name, danceability, energy, liveness, valence,
-             track_popularity)
+      select(
+        album_name, track_name, danceability, energy, liveness, valence,
+        track_popularity
+      )
     return(popularity_plot(data, input$pop_var))
   })
-  
-  output$playlist_table <- renderPlotly({
 
-    if(input$playlist_type == "dance") {
+  output$playlist_table <- renderPlotly({
+    if (input$playlist_type == "dance") {
       mood <- dance(input$username)
-    } else if (input$playlist_type == "chill"){
+    } else if (input$playlist_type == "chill") {
       mood <- chill(input$username)
-    } else{
+    } else {
       mood <- simp(input$username)
     }
-    
+
     return(createTable(mood))
   })
 })
