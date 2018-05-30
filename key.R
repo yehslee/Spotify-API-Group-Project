c561f2bdd883555c8b9c3cade8ba3de885a18ade
yehslee <yehslee@uw.edu>
2018-05-30 14:12:19 -0700

    Do key gitignore

--- key.R
+++
@@ -1,7 +0,0 @@
-library(spotifyr)
-library(dplyr)
-Sys.setenv(SPOTIFY_CLIENT_ID = '6fba02969ff44c8892e1e562ff7d2ce3')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = '42f0606c30ec444195c5687a1d03550d')
-access_token <- get_spotify_access_token()
-
- 
